b6c6e1b71df610622e00f9f9a4804822c66d535f
devjoaocorrea <joaomarcoscorrea0707@gmail.com>
2024-10-18 17:19:33 -0300

    fix: Remove *.app do .gitignore para adicionar o projeto de dependencias

---
+++ BancoChu.App/Container.cs
@@ -0,0 +1,27 @@
+﻿using BancoChu.Domain.Handlers;
+using BancoChu.Domain.Interfaces.Handlers;
+using BancoChu.Domain.Interfaces.Repositories;
+using BancoChu.Infra.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BancoChu.App;
+
+public static class Container
+{
+    public static void AddServices(this IServiceCollection services)
+    {
+        AddHandlers(services);
+        AddRepositories(services);
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<ITransacoesRepository, TransacoesRepository>();
+        services.AddScoped<IContasRepository, ContasRepository>();
+    }
+
+    private static void AddHandlers(IServiceCollection services)
+    {
+        services.AddScoped<ITransacoesHandler, TransacoesHandler>();
+    }
+}
