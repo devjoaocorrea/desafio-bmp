412543a90121462dcde00a7db259047fedc9969b
devjoaocorrea <joaomarcoscorrea0707@gmail.com>
2024-10-20 18:56:53 -0300

    feat: Implementa os testes unitarios

---
+++ BancoChu.Tests/Contas/ContasTests.cs
@@ -0,0 +1,121 @@
+﻿using BancoChu.Domain.Entidades;
+using BancoChu.Domain.Entidades.Validators;
+using FluentValidation.TestHelper;
+
+namespace BancoChu.Tests.Contas;
+
+public class ContasTests
+{
+	private readonly ContaValidator _validator;
+
+	public ContasTests()
+	{
+		_validator = new ContaValidator();
+	}
+
+	[Fact]
+	public void Deve_RetornarErro_Quando_TitularNaoForInformado()
+	{
+		// Arrange
+		var command = new Conta { Titular = "" };
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldHaveValidationErrorFor(x => x.Titular)
+			  .WithErrorMessage("O titular deve ser preenchido");
+	}
+
+	[Fact]
+	public void Deve_RetornarErro_Quando_TitularNaoTiverEntre3e20Caracteres()
+	{
+		// Arrange
+		var command = new Conta { Titular = "Jo" };
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldHaveValidationErrorFor(x => x.Titular)
+			  .WithErrorMessage("O titular deve ter entre 3 até 20 caracteres");
+	}
+
+	[Fact]
+	public void Nao_DeveRetornarErro_Quando_TitularForValido()
+	{
+		// Arrange
+		var command = new Conta { Titular = "Joao Silva" };
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldNotHaveValidationErrorFor(x => x.Titular);
+	}
+
+	[Fact]
+	public void Deve_RetornarErro_Quando_AgenciaNaoForInformada()
+	{
+		// Arrange
+		var command = new Conta { Agencia = "" };
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldHaveValidationErrorFor(x => x.Agencia)
+			  .WithErrorMessage("A agência deve ser informado");
+	}
+
+	[Fact]
+	public void Nao_DeveRetornarErro_Quando_AgenciaForInformada()
+	{
+		// Arrange
+		var command = new Conta { Agencia = "1234" };
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldNotHaveValidationErrorFor(x => x.Agencia);
+	}
+
+	[Fact]
+	public void Deve_RetornarErro_Quando_NumeroContaNaoForInformado()
+	{
+		// Arrange
+		var command = new Conta { NumeroConta = "" };
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldHaveValidationErrorFor(x => x.NumeroConta)
+			  .WithErrorMessage("O número da conta deve ser informado");
+	}
+
+	[Fact]
+	public void Nao_DeveRetornarErro_Quando_NumeroContaForInformado()
+	{
+		// Arrange
+		var command = new Conta { NumeroConta = "123456" };
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldNotHaveValidationErrorFor(x => x.NumeroConta);
+	}
+
+	[Fact]
+	public void Deve_RetornarErro_Quando_SaldoForNegativo()
+	{
+		// Arrange
+		var command = new Conta();
+		command.AlterarSaldo(-1);
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldHaveValidationErrorFor(x => x.Saldo)
+			  .WithErrorMessage("O valor do saldo não pode ser negativo ao criar a conta");
+	}
+
+	[Fact]
+	public void Nao_DeveRetornarErro_Quando_SaldoForPositivo()
+	{
+		// Arrange
+		var command = new Conta();
+		command.AlterarSaldo(100);
+
+		// Act e Assert
+		var result = _validator.TestValidate(command);
+		result.ShouldNotHaveValidationErrorFor(x => x.Saldo);
+	}
+}
---
+++ BancoChu.Tests/Transacoes/TransacoesTest.cs
@@ -0,0 +1,97 @@
+﻿using BancoChu.Domain.Entidades.Validators;
+using BancoChu.Dto.Commands;
+using FluentValidation.TestHelper;
+
+namespace BancoChu.Tests.Transacoes;
+
+public class TransacoesTests
+{
+    private readonly TransacaoValidator _validator;
+
+    public TransacoesTests()
+    {
+        _validator = new();
+
+    }
+
+    [Fact]
+    public void Deve_RetornarErro_Quando_ValorForZero()
+    {
+        // Arrange
+        var command = new TransacaoCommand { Valor = 0 };
+
+        // Act e Assert
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Valor)
+              .WithErrorMessage("Não pode realizar uma transação com o valor zerado");
+    }
+
+    [Fact]
+    public void Deve_RetornarErro_Quando_ValorForNegativo()
+    {
+        // Arrange
+        var command = new TransacaoCommand { Valor = -20 };
+
+        // Act e Assert
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.Valor)
+              .WithErrorMessage("Não pode realizar uma transação com o valor zerado");
+    }
+
+    [Fact]
+    public void Nao_Deve_RetornarErro_Qundo_ValorForPositivo()
+    {
+        // Arrange
+        var command = new TransacaoCommand { Valor = 100 };
+
+        // Act e Assert
+        var result = _validator.TestValidate(command);
+        result.ShouldNotHaveValidationErrorFor(x => x.Valor);
+    }
+
+    [Fact]
+    public void Deve_RetornarErro_Quando_ContaOrigemIdNaoForInformada()
+    {
+        // Arrange
+        var command = new TransacaoCommand { ContaOrigemId = Guid.Empty };
+
+        // Act e Assert
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.ContaOrigemId)
+              .WithErrorMessage("Conta de origem não informada");
+    }
+
+    [Fact]
+    public void Nao_DeveRetornarErro_Quando_ContaOrigemIdForInformada()
+    {
+        // Arrange
+        var command = new TransacaoCommand { ContaOrigemId = Guid.NewGuid() };
+
+        // Act e Assert
+        var result = _validator.TestValidate(command);
+        result.ShouldNotHaveValidationErrorFor(x => x.ContaOrigemId);
+    }
+
+    [Fact]
+    public void Deve_RetornarErro_Quando_ContaDestinoIdNaoForInformada()
+    {
+        // Arrange
+        var command = new TransacaoCommand { ContaDestinoId = Guid.Empty };
+
+        // Act e Assert
+        var result = _validator.TestValidate(command);
+        result.ShouldHaveValidationErrorFor(x => x.ContaDestinoId)
+              .WithErrorMessage("Conta de destino não informada");
+    }
+
+    [Fact]
+    public void Nao_DeveRetornarErro_Quando_ContaDestinoIdForInformada()
+    {
+        // Arrange
+        var command = new TransacaoCommand { ContaDestinoId = Guid.NewGuid() };
+
+        // Act e Assert
+        var result = _validator.TestValidate(command);
+        result.ShouldNotHaveValidationErrorFor(x => x.ContaDestinoId);
+    }
+}
